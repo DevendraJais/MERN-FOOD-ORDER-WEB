e0b4bf124827bc1360655ec47014de1ec434664e
DevendraJais <devendra7jaiswal@gmail.com>
2025-11-20 13:45:20 +0530

    added the backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE = import.meta.env.VITE_API_URL ||  "https://mern-food-order-web.onrender.com";
 
 export const API = axios.create({
     baseURL: `${BASE}/api`,
